1bfa018f7cb8d133ea4047c2c1055fbd7d1fd180
Joseph Stelley <joseph.stelley@example.com>
2026-04-27 17:14:19 -0500

    Prompt 4

--- GameController.java
+++ GameController.java
@@ -1,9 +1,14 @@
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class GameController {
+public class GameController implements KeyListener {
     private GameModel model;
     private GameView view;
+    private JFrame frame;
+    private Timer timer;
 
     public GameController() {
         model = new GameModel();
@@ -11,12 +16,25 @@ public class GameController {
     }
 
     private void startGame() {
-        JFrame frame = new JFrame("Space Invaders");
+        frame = new JFrame("Space Invaders");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(view);
-        frame.setSize(800, 600);
+        frame.setSize(GameModel.WIDTH, GameModel.HEIGHT);
         frame.setLocationRelativeTo(null);
+        frame.addKeyListener(this);
         frame.setVisible(true);
+
+        timer = new Timer(16, e -> {
+            if (model.getLives() > 0) {
+                model.update();
+                view.repaint();
+            } else {
+                ((Timer) e.getSource()).stop();
+                view.repaint();
+            }
+        });
+
+        timer.start();
     }
 
     public static void main(String[] args) {
@@ -24,5 +42,32 @@ public class GameController {
             GameController controller = new GameController();
             controller.startGame();
         });
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (model.getLives() <= 0) {
+            return;
+        }
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            model.movePlayerLeft();
+        } else if (key == KeyEvent.VK_RIGHT) {
+            model.movePlayerRight();
+        } else if (key == KeyEvent.VK_SPACE) {
+            model.firePlayerBullet();
+        }
+
+        view.repaint();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
     }
 }
